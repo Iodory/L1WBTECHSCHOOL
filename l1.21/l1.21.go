d58f4ee84f2016732d056252eb2056acbab5c373
Ryo <nesterov.timka05@mail.ru>
2025-08-12 15:14:57 +0300

    l1.21

--- l1.21/l1.21.go
+++ l1.21/l1.21.go
@@ -20,9 +20,31 @@ func (a *AdaptDog) Speak() string {
 	return a.Dog.Bark()
 }
 
+type Cat struct{}
+
+func (c *Cat) Meow() string {
+	return "Meow!!!!"
+}
+
+type AdaptCat struct {
+	Cat *Cat
+}
+
+func (a *AdaptCat) Speak() string {
+	return a.Cat.Meow()
+}
+
+func SomeFunc(s Speaker) {
+	fmt.Println("Govorit:", s.Speak())
+}
+
 func main() {
 	sobaka := &Dog{}
 	coolSobaka := &AdaptDog{sobaka}
 
-	fmt.Println(coolSobaka.Speak())
+	kot := &Cat{}
+	coolKot := &AdaptCat{kot}
+
+	SomeFunc(coolSobaka)
+	SomeFunc(coolKot)
 }
